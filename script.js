b90fa1b8f3ec1b1d9ab40e72d8aa0879155f8a44
Mike Lord <73435010+Lormik@users.noreply.github.com>
2020-10-27 09:51:58 +0000

    update direction and distance calculators

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 
 // random distance total
-// distance is in km and to 1dp
 
+// distance is in km and to 1dp
+// function to create a random distance
 const distance = () => {
-    let pre = Math.floor(Math.random() * 100);
+    let pre = Math.floor(Math.random() * 50);
     let post = Math.floor(Math.random() * 10)/10;
     let num = pre + post;
     return num;
@@ -19,25 +20,40 @@ const distance = () => {
 // pick a direction to go
 const options = ['left', 'right', 'forward', 'go back'];
 const way = () => {
-    return way[Math.floor(Math.random() * options.length)]
+    return options[Math.floor(Math.random() * options.length)]
 }
 
 //input is the distance to travel
 const directions = (num) => {
     // Create distance remaining counter rem
     let rem = num - 0;
-    
+
     // Create empty array to store directions and lengths
     let route = [];
 
     // if dist rem < 0.5 pick direction and go that far
     if (rem < 0.5) {
-        way()
+        // direction
+        way();
+        // distance
+        Math.floor(Math.random() * (rem * 10))/10;
+
+    } else if (rem >= 0.5) {
+        // direction
+        route.push(way());
+        
+        // distance
+
     }
 
     // if dist rem > 0.5 pick random distance between 0 and rem 
 
+
     // join array to form output of directions
+    // first how far you want to go
+    // then step by step directionss
+    // final statement saying oh no we are lost
+
 }
 
 
